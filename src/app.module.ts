ebc6e9ad8f6aa4212a28d56aa6ec03d4a507b3f7
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-06-02 22:27:47 +0300

    feat: created articles module and rest endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArticlesModule } from './articles/articles.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
+    ArticlesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
