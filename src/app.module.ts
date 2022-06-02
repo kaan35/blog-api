2c2b24ca3d728efb345885547e6ae792c6d3a5af
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-06-02 22:28:04 +0300

    feat: created authors module and rest endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArticlesModule } from './articles/articles.module';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DATABASE_URL),
     ArticlesModule,
+    AuthorsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
