6e6b6540825dec08815d236c3a2124cad3a49b5c
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-06-03 23:21:45 +0300

    feat: added author model and updated findAll method to return query result from database

--- src/authors/authors.service.ts
+++ src/authors/authors.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Author, AuthorDocument } from './schemas/authors.schema';
 
 @Injectable()
 export class AuthorsService {
+  constructor(
+    @InjectModel(Author.name) private authorModel: Model<AuthorDocument>,
+  ) {}
+
   create(createAuthorDto: CreateAuthorDto) {
     return 'This action adds a new author';
   }
 
   findAll() {
-    return `This action returns all authors`;
+    return this.authorModel.find().exec();
   }
 
   findOne(id: number) {
