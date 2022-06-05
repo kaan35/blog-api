581c2d8db4dee0f902ef211b7363498199bf85ca
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-06-05 23:11:23 +0300

    test(authors) : added missing config and mongoose module imports

--- src/authors/authors.controller.spec.ts
+++ src/authors/authors.controller.spec.ts
@@ -1,19 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorsController } from './authors.controller';
 import { AuthorsService } from './authors.service';
+import { Author, AuthorSchema } from './schemas/authors.schema';
 
 describe('AuthorsController', () => {
   let controller: AuthorsController;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DATABASE_URL),
+        MongooseModule.forFeature([
+          { name: Author.name, schema: AuthorSchema },
+        ]),
+      ],
       controllers: [AuthorsController],
       providers: [AuthorsService],
     }).compile();
-
     controller = module.get<AuthorsController>(AuthorsController);
   });
-
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
--- src/authors/authors.service.spec.ts
+++ src/authors/authors.service.spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorsService } from './authors.service';
+import { Author, AuthorSchema } from './schemas/authors.schema';
 
 describe('AuthorsService', () => {
   let service: AuthorsService;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DATABASE_URL),
+        MongooseModule.forFeature([
+          { name: Author.name, schema: AuthorSchema },
+        ]),
+      ],
       providers: [AuthorsService],
     }).compile();
-
     service = module.get<AuthorsService>(AuthorsService);
   });
-
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
