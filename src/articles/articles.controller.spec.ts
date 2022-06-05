e80038acaa39263e261093a3f32daf419059ce06
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-06-05 23:10:53 +0300

    test(articles) : added missing config and mongoose module imports

--- src/articles/articles.controller.spec.ts
+++ src/articles/articles.controller.spec.ts
@@ -1,19 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ArticlesController } from './articles.controller';
 import { ArticlesService } from './articles.service';
+import { Article, ArticleSchema } from './schemas/articles.schema';
 
 describe('ArticlesController', () => {
   let controller: ArticlesController;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DATABASE_URL),
+        MongooseModule.forFeature([
+          { name: Article.name, schema: ArticleSchema },
+        ]),
+      ],
       controllers: [ArticlesController],
       providers: [ArticlesService],
     }).compile();
-
     controller = module.get<ArticlesController>(ArticlesController);
   });
-
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
--- src/articles/articles.service.spec.ts
+++ src/articles/articles.service.spec.ts
@@ -1,17 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ArticlesService } from './articles.service';
+import { Article, ArticleSchema } from './schemas/articles.schema';
 
 describe('ArticlesService', () => {
   let service: ArticlesService;
-
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DATABASE_URL),
+        MongooseModule.forFeature([
+          { name: Article.name, schema: ArticleSchema },
+        ]),
+      ],
       providers: [ArticlesService],
     }).compile();
-
     service = module.get<ArticlesService>(ArticlesService);
   });
-
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
