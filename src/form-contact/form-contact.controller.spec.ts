698be920715e4aeba17de64e7d42dfda9b2cfb06
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-07-02 23:38:53 +0300

    test(form) : added missing config and mongoose module imports

--- src/form-contact/form-contact.controller.spec.ts
+++ src/form-contact/form-contact.controller.spec.ts
@@ -1,13 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FormContactController } from './form-contact.controller';
 import { FormContactService } from './form-contact.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  Form_Contact_Message,
+  FormContactMessageSchema,
+} from './schemas/form-contact-messages.schema';
+import { ValidateService } from '../utils/validate/validate.service';
 
 describe('FormContactController', () => {
   let controller: FormContactController;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DATABASE_URL),
+        MongooseModule.forFeature([
+          { name: Form_Contact_Message.name, schema: FormContactMessageSchema },
+        ]),
+      ],
       controllers: [FormContactController],
-      providers: [FormContactService],
+      providers: [FormContactService, ValidateService],
     }).compile();
     controller = module.get<FormContactController>(FormContactController);
   });
--- src/form-contact/form-contact.service.spec.ts
+++ src/form-contact/form-contact.service.spec.ts
@@ -1,11 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { FormContactService } from './form-contact.service';
+import {
+  Form_Contact_Message,
+  FormContactMessageSchema,
+} from './schemas/form-contact-messages.schema';
+import { ValidateService } from '../utils/validate/validate.service';
 
 describe('FormContactService', () => {
   let service: FormContactService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FormContactService],
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DATABASE_URL),
+        MongooseModule.forFeature([
+          { name: Form_Contact_Message.name, schema: FormContactMessageSchema },
+        ]),
+      ],
+      providers: [FormContactService, ValidateService],
     }).compile();
     service = module.get<FormContactService>(FormContactService);
   });
