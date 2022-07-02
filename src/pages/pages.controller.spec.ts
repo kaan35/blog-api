c94c8096ec6622bd32dcdb5ad5584ec2083c1545
Kaan Karaman <25592633+kaan35@users.noreply.github.com>
2022-07-02 23:40:20 +0300

    test(pages) : added missing config and mongoose module imports

--- src/pages/pages.controller.spec.ts
+++ src/pages/pages.controller.spec.ts
@@ -1,11 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PagesController } from './pages.controller';
 import { PagesService } from './pages.service';
+import { Page, PagesSchema } from './schemas/pages.schema';
 
 describe('PagesController', () => {
   let controller: PagesController;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DATABASE_URL),
+        MongooseModule.forFeature([{ name: Page.name, schema: PagesSchema }]),
+      ],
       controllers: [PagesController],
       providers: [PagesService],
     }).compile();
--- src/pages/pages.service.spec.ts
+++ src/pages/pages.service.spec.ts
@@ -1,10 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PagesService } from './pages.service';
+import { Page, PagesSchema } from './schemas/pages.schema';
 
 describe('PagesService', () => {
   let service: PagesService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(process.env.DATABASE_URL),
+        MongooseModule.forFeature([{ name: Page.name, schema: PagesSchema }]),
+      ],
       providers: [PagesService],
     }).compile();
     service = module.get<PagesService>(PagesService);
